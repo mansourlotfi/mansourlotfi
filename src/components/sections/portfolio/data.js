84e240c201d4545bc8e80793f4d35ece5627cd34
mansourlotfi <mansourlotfi@gmail.com>
2023-05-28 17:13:04 +0330

    Fix links

--- src/components/sections/portfolio/data.js
+++ src/components/sections/portfolio/data.js
@@ -6,7 +6,7 @@ const data = [
     title: "Project Title One (UIUX)",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
     demo: "https://github.com/mansourlotfi",
-    github: "https://github.com/egattor",
+    github: "https://github.com/mansourlotfi",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const data = [
     title: "Project Title Two (Frontend)",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
     demo: "https://github.com/mansourlotfi",
-    github: "https://github.com/egattor",
+    github: "https://github.com/mansourlotfi",
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const data = [
     title: "Project Title Three (Frontend)",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
     demo: "https://github.com/mansourlotfi",
-    github: "https://github.com/egattor",
+    github: "https://github.com/mansourlotfi",
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ const data = [
     title: "Project Title Four (UIUX)",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
     demo: "https://github.com/mansourlotfi",
-    github: "https://github.com/egattor",
+    github: "https://github.com/mansourlotfi",
   },
   {
     id: 5,
@@ -42,7 +42,7 @@ const data = [
     title: "Project Title Five (UIUX)",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! ",
     demo: "https://github.com/mansourlotfi",
-    github: "https://github.com/egattor",
+    github: "https://github.com/mansourlotfi",
   },
   {
     id: 6,
@@ -51,7 +51,7 @@ const data = [
     title: "Project Title Six (Frontend)",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
     demo: "https://github.com/mansourlotfi",
-    github: "https://github.com/egattor",
+    github: "https://github.com/mansourlotfi",
   },
   {
     id: 7,
@@ -60,7 +60,7 @@ const data = [
     title: "Project Title Seven (Frontend)",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
     demo: "https://github.com/mansourlotfi",
-    github: "https://github.com/egattor",
+    github: "https://github.com/mansourlotfi",
   },
   {
     id: 8,
@@ -69,7 +69,7 @@ const data = [
     title: "Project Title Eight (Backend)",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
     demo: "https://github.com/mansourlotfi",
-    github: "https://github.com/egattor",
+    github: "https://github.com/mansourlotfi",
   },
   {
     id: 9,
@@ -78,7 +78,7 @@ const data = [
     title: "Project Title Nine (Frontend)",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
     demo: "https://github.com/mansourlotfi",
-    github: "https://github.com/egattor",
+    github: "https://github.com/mansourlotfi",
   },
 ];
 
